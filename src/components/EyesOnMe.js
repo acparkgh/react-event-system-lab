a61f70337a8d801c8266dab65a2cb4fb7b8cd07c
acparkgh <acpark123@gmail.com>
2021-01-08 17:34:35 -0500

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,27 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+  
+  render() {
+    return (
+      <div>
+        <button onFocus={ () => console.log('Good!') }
+                onBlur={ () => console.log('Hey! Eyes on me!') }
+        >
+          ClickMe!
+        </button>  
+      </div>
+    )
+  }
+
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,25 @@
 // Code Keypad Component Here
+import React from 'react';
+
+class Keypad extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+  
+  render() {
+    return (
+      <div>
+        <input type="password" 
+               onKeyUp={ () => console.log('Entering password...') }
+        />
+      </div>
+    )
+  }
+
+}
+
+export default Keypad
